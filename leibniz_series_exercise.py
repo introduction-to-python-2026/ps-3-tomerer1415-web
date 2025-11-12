f6b0fb90e5019082aead8b895d7d9f3a2eba1de2
tomerer1415-web <tomerer1415@gmail.com>
2025-11-12 13:59:55 +0200

    Update leibniz_series_exercise.py

--- leibniz_series_exercise.py
+++ leibniz_series_exercise.py
@@ -1,5 +1,5 @@
 def approximate_pi(n_terms):
-    pi_approx = 0
+    pi = 0
     for i in range(n_terms):
-        pi_approx += ((-1) ** k) / (2 * k + 1)
-    return 4 * pi_approx
+        pi += ((-1)**i)/(2*i + 1)
+    return (4 * pi)
